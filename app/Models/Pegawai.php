90d9d3058f503f2e091cb8adf8804fc6ae950a50
Ahsani Fadhli Ilahi <ahsanifadhliilahi@mail.ugm.ac.id>
2025-12-17 11:04:59 +0700

    nambahin softdelete di bagian pegawai

--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pegawai extends Model
 {
     protected $table = 'pegawai';
+
+    use HasFactory, SoftDeletes;
 
     public function pegawai()
     {
